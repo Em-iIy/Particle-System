577d5e4516f3a45e4d5542c94961af55cb2080b3
Emily Winnink <emily.winnink@gmail.com>
2025-03-10 17:24:29 +0100

    Catch exception on compile error

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,6 @@ int main(int argc, char **argv)
 		srand(std::time(NULL));
 		config.load(argv[2]);
 		app.init(config);
-		std::cout << "init" << std::endl;
 		while (app.is_running())
 		{
 			app.update();
@@ -36,6 +35,10 @@ int main(int argc, char **argv)
 	catch(const std::runtime_error &e)
 	{
 		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+	catch(const std::exception &e)
+	{
 		return (1);
 	}
 	return (0);
